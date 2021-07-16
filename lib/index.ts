7f40c6e7b0f1e39d7261abc38e64d15596cca721
Josua Blejeru <37913833+josuablejeru@users.noreply.github.com>
2021-07-16 19:53:16 +0200

    Initial commit

---
+++ lib/index.ts
@@ -0,0 +1,14 @@
+import * as cdk from '@aws-cdk/core';
+
+export interface AwsOpenapiUiProps {
+  // Define construct properties here
+}
+
+export class AwsOpenapiUi extends cdk.Construct {
+
+  constructor(scope: cdk.Construct, id: string, props: AwsOpenapiUiProps = {}) {
+    super(scope, id);
+
+    // Define construct contents here
+  }
+}
