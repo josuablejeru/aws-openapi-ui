fcee152e1bf1f000d85f3e8235690f7bb278163c
Josua Blejeru <37913833+josuablejeru@users.noreply.github.com>
2021-07-22 15:30:09 +0200

    create webpack config

---
+++ webpack.config.js
@@ -0,0 +1,58 @@
+const path = require('path');
+const NodePolyfillPlugin = require("node-polyfill-webpack-plugin")
+const CopyPlugin = require("copy-webpack-plugin")
+const nodeExternals = require('webpack-node-externals')
+
+module.exports = {
+  entry: './lib/index.ts',
+  target: 'node',
+  entry: {
+    app: './frontend/index.ts'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.yaml$/,
+        use: [
+          { loader: 'json-loader' },
+          { loader: 'yaml-loader' }
+        ]
+      },
+      {
+        test: /\.css$/,
+        use: [
+          { loader: 'style-loader' },
+          { loader: 'css-loader' },
+        ]
+      }
+    ],
+  },
+  resolve: {
+    extensions: ['.ts', '.js'],
+  },
+  output: {
+    filename: 'index.js',
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: '/',
+    libraryTarget: 'commonjs',
+    globalObject: 'this',
+  },
+  plugins: [
+    new NodePolyfillPlugin(),
+    new CopyPlugin({
+        patterns: [
+          { from: "swagger-ui", to: "swagger-ui" }
+        ],
+      }),
+      new HtmlWebpackPlugin({
+        template: 'index.html'
+      })
+  ],
+  // in order to ignore all modules in node_modules folder 
+  externals: [nodeExternals()],
+};
